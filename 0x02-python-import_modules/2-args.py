d7b50a629c63398a27916396962d00182e9d6813
G-omar-H <omarghazi12@gmail.com>
2023-09-07 18:55:07 +0100

    0x02 python project

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    i = len(argv) - 1
-    if i == 0:
-        print("{} arguments.".format(i))
-    else:
-        j = 1
-        print("{} arguments:".format(i))
-        while j <= i:
-            print("{}: {}".format(j, argv[j]))
-            j += 1
+from sys import argv
+i = len(argv) - 1
+if i == 0:
+    print("{} arguments.".format(i))
+elif i:
+    j = 1
+    print("{} arguments:".format(i))
+    while j <= i:
+        print("{}: {}".format(j, argv[j]))
+        j += 1
