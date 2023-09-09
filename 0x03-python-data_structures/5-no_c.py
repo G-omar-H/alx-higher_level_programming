0c5098cd334a83cf0523f0b8c1c0d62b30b4cd78
G-omar-H <omarghazi12@gmail.com>
2023-09-09 18:58:28 +0100

    0x03 python project

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    #while i < len(my_string):
-     #       if my_string[i]  not in  ['c', 'C']:
-     #          new_string + my_string[i]
-     #    new_string = my_string for my_string[i] not in ['c', 'C']
-    return my_string.translate(delete='cC')
+    temp = my_string.translate({ord('c'): None})
+    new_string = temp.translate({ord('C'): None})
+    return new_string
