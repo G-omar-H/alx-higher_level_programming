0036086a2d6ae1b9198f084ed2ca75430bcabcf0
G-omar-H <omarghazi12@gmail.com>
2023-09-09 19:39:21 +0100

    0x03 python project

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        
+    tp1 = check_tuplen(tuple_a)
+    tp2 = check_tuplen(tuple_b)
+    tuple = (tp1[0] + tp2[0], tp1[1] + tp2[1])
+    return tuple
+
+
+def check_tuplen(tup=()):
+    if len(tup) < 2:
+        if len(tup) < 1:
+            temp = (0, 0)
+            return temp
+        else:
+            temp = (tup[0], 0)
+            return temp
+    else:
+        return tup
