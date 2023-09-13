43c2e024370920cc8d33e74667317a5065f8e8ad
G-omar-H <omarghazi12@gmail.com>
2023-09-13 14:04:34 +0100

    0x04 python more data structure 4st task

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -5,6 +5,6 @@ def common_elements(set_1, set_2):
     for item in set_1:
         for item2 in set_2:
             if item == item2:
-                _list += item
-    _list = set(_list)
-    return _list
+                _list.append(item)
+    _set = set(_list)
+    return _set
