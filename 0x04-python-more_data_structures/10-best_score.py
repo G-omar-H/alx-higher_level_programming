c6be1e4cd750de6f63d9b633e3310ca5d23d351e
G-omar-H <omarghazi12@gmail.com>
2023-09-13 22:55:35 +0100

    0x04 python more data structure 10st task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    if not a_dictionary:
+        return None
+    var = 0
+    item = ""
+    for key, value in a_dictionary.items():
+        if value > var:
+            var = value
+            item = key
+    return item
