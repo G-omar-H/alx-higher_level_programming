963f07c7aa44d7d8670c2af53ccdf4a46719ceb2
G-omar-H <omarghazi12@gmail.com>
2023-09-26 00:02:50 +0100

    0x05 exception handled

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-class Messageprompt(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
-
-
 def raise_exception_msg(message=""):
-    raise Messageprompt(message)
+    raise Exception(message)
