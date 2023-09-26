46deaf14970ef494f3fa53640711b884430d9b63
G-omar-H <omarghazi12@gmail.com>
2023-09-26 23:02:08 +0100

    0x06 classy python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,16 +9,7 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
