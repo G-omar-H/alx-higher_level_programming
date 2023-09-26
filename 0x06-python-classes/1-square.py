2a5a058b020dbc2302ee24a46e3e75c1ec06e020
G-omar-H <omarghazi12@gmail.com>
2023-09-26 20:11:43 +0100

    0x06 classy python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defining a square by size"""
+
+
+class Square:
+    """square classe creating
+    Attributes:
+        size (int): private attribute defining the square size
+    """
+    def __init__(self, size="0"):
+        self.__size = size
