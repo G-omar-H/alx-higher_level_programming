7b2d16133abf4d7c73a51985939a57cb932d2d57
G-omar-H <omarghazi12@gmail.com>
2023-09-27 08:32:57 +0100

    0x06 classy python

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""defining a square by size"""
+
+
+class Square:
+    """square classe creating
+    Attributes:
+        size (int): private attribute defining the square size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print()
