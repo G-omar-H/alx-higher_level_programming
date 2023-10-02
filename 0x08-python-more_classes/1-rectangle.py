80c755e4bc8a1e9880ddbf4b552cc3042c82676c
G-omar-H <omarghazi12@gmail.com>
2023-10-02 12:06:11 +0100

    class height,width private attributes implemented

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width must be => 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -34,6 +34,6 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be => 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
