ca5512dec640af88f49888cd5d68da07bd0b0e86
G-omar-H <omarghazi12@gmail.com>
2023-10-02 12:41:31 +0100

    class height,width private attributes implemented

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """defining a class of a rectangle"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
