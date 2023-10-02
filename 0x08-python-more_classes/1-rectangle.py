f6d5d96463e3baf32b28d0b0d26a1e633a5f63e8
G-omar-H <omarghazi12@gmail.com>
2023-10-02 11:56:46 +0100

    class height,width private attributes implemented

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""""creating empty class"""
+
+
+class Rectangle:
+    """"empty class created!"""
+
+    def __init__(self, width=0, height=0):
+        """defining a class of a rectangle"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """"set a vlue to a private instance width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be => 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be => 0")
+        else:
+            self.__height = value
