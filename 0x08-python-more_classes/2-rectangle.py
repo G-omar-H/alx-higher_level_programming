fad4fdc80449b2802cc3e9a81a40ef40c03ace65
G-omar-H <omarghazi12@gmail.com>
2023-10-02 12:59:18 +0100

    class area, perimeter methods implemented

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+""""creating empty class"""
+
+
+class Rectangle:
+    """"empty class created!"""
+
+    def __init__(self, width=0, height=0):
+        """defining a class of a rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"set a vlue to a private instance width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == self.__height == 0:
+            return 0
+        else:
+            return 2*self.__width + 2*self.height
