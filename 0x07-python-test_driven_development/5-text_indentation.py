3da19ad4343cb1f21bdd6653306fa65c9104d2f8
G-omar-H <omarghazi12@gmail.com>
2023-10-06 20:34:52 +0100

    test driven devops!

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+test driven devops learning project
+python programing language
+ALX software engineering
+
+"""
+
+
+def text_indentation(text):
+    """function that prints a text"""
+
+    i = 0
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    txt = text + '_'
+
+    while (txt[i] == " "):
+        i += 1
+
+    while (i < len(text)):
+        print("{}".format(txt[i]), end="")
+
+        if txt[i] in {':', '.', '?'}:
+            print("{}".format("\n"))
+
+            while txt[i + 1] == ' ':
+                i += 1
+        i += 1
