6e09db7f3fcaa273f2de666497e44af584ab379b
G-omar-H <omarghazi12@gmail.com>
2023-10-06 11:51:36 +0100

    doctest implementations

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,21 @@
-#!/usr/bin/pyton3
-""""
-add function that return the sum of two integers
+#!/usr/bin/python3
+"""
+    Miami
 """
 
 
 def add_integer(a, b=98):
     """
-    proceed the addiction of two integers
-    @parameters
-    a: first integer
-    b: second integer
-    @return
-        the sum of the two integers
-    @exception
-        TypeError if one of the two integers is not an integer
+        DOES
+        :param a: aaaaa
+        :type a: aaa
+        :param b: bbbbb
+        :type b: bbbb
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
+
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
