ee1daf69d77c55a007ef3c756434c42c32860c17
G-omar-H <omarghazi12@gmail.com>
2023-10-09 11:04:00 +0100

    class inherited!

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ALX inhritence project
+"""
+
+
+class MyList(list):
+    """Implemets sorted printing for the built=in list class"""
+
+    def print_sorted(self):
+        """prints a list in sorted ascending order"""
+        super().sort()
+        return self
