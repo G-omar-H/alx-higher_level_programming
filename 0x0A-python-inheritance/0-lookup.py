b3bb6aead2f357a182b67326058cc2b2829ac296
G-omar-H <omarghazi12@gmail.com>
2023-10-09 09:56:03 +0100

    class inherited!

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+alx inheritance project
+"""
+
+
+def lookup(obj):
+    """
+    function to return a list of available attributes
+    && methods of an object.
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+0x0A  inheritance
+Python programming language
+ALX software engineering.
