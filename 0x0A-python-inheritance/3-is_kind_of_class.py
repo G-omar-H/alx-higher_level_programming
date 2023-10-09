bbdcf85c8a1db996f1bdeb1ec25d0e1f6888f73a
G-omar-H <omarghazi12@gmail.com>
2023-10-09 12:12:28 +0100

    class inherited!

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
-#!usr/bin/python3
-"""Class inheritance class-chacking function"""
+#!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
 
 
 def is_kind_of_class(obj, a_class):
