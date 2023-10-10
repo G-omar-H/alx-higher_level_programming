771439c2e17dfc24acaff6b70d56f9ba1080831c
G-omar-H <omarghazi12@gmail.com>
2023-10-10 21:14:01 +0100

    json load, add, save implemented!

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""ALX file I/O learning project or python programming language"""
+"""
+ALX file I/O learning project
+"""
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__("6-load_from_json_file").load_from_json_file
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, "add_item.json")
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(my_list, "add_item.json")
