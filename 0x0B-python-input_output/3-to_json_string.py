1833c79c7dd64a714409896acc4a9558c7acd09a
G-omar-H <omarghazi12@gmail.com>
2023-10-10 18:54:43 +0100

    Json serialazed object input!

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,12 @@ ALX file I/O learning project
 
 
 def to_json_string(my_obj):
+    """
+    function that serialize an  object into json format
+    args:
+        @my_obj(string): object to serialize
+        return json representation of the object
+    """
     import json
 
     return json.dumps(my_obj)
