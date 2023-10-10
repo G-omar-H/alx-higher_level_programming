d497873149f8cca253df33b11d5c35becbc18ba8
G-omar-H <omarghazi12@gmail.com>
2023-10-10 20:15:34 +0100

    json load, add, save implemented!

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,20 @@ ALX file I/O learning project
 """
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+"""
+function that writes the Json representation of an object  into a file
+args:
+    @my_obj: object to serialize as Json
+    @filename: absolute or relative path to the file
+    Return: None
+"""
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""
+function that creates an object from a json file
+args:
+    @filename: absolute or relative path string to a file
+Return: object decoded from json file
+    """
 import sys
 import json
 
