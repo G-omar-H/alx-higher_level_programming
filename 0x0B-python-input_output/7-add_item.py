1c762f62a6bf8f3f66b4f14c1de1a8261a9aefa2
G-omar-H <omarghazi12@gmail.com>
2023-10-10 20:50:55 +0100

    json load, add, save implemented!

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,8 @@
 """
 ALX file I/O learning project
 """
+import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     """
@@ -11,16 +13,13 @@ if __name__ == "__main__":
         @filename: absolute or relative path to the file
         Return: None
     """
-    load_from_json_file = \
-        __import__("6-load_from_json_file").load_from_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
     """
     function that creates an object from a json file
     args:
         @filename: absolute or relative path string to a file
     Return: object decoded from json file
     """
-import sys
-
 try:
     my_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
