f21be542111c97e909f9d3cc39483d065db8d641
G-omar-H <omarghazi12@gmail.com>
2023-10-19 10:54:37 +0100

    CSV serialization/deserialization implemened!

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,9 +13,10 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        self.id = id
-        Base.__nb_objects += 1
+        if id is not None:
+            self.id = id
         if id is None:
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     @staticmethod
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++
@@ -1,5 +0,0 @@
-"test_base.py"
-#!/usr/bin/python3
-
-
-from models.base import Base
