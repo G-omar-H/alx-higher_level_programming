50e7a3de9517c054043f725c5d0e025b5986511c
G-omar-H <omarghazi12@gmail.com>
2023-12-07 14:59:35 +0100

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name varchar(256),
 score INT
-)
+);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
