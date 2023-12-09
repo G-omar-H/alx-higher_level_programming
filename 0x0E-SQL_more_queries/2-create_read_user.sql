6245edef1e78254a880820d4ade9b6612cc17f65
G-omar-H <omarghazi12@gmail.com>
2023-12-09 09:29:04 +0100

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,3 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT  ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
-REVOKE SELECT ON `htbtn_0d_2`.* from  `user_0d_2`@`localhost`;
