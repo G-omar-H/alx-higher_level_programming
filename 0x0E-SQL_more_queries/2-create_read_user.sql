ee6a3d953fe6acaaf9bfd695ee9830ceb5f4a8ff
G-omar-H <omarghazi12@gmail.com>
2023-12-09 09:28:28 +0100

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- script to creat user_0d_2 ans the database htbn_0d_2 on the MYSQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT  ON `htbtn_0d_2`.* TO `user_0d_2`@`localhost`;
+GRANT SELECT  ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
+REVOKE SELECT ON `htbtn_0d_2`.* from  `user_0d_2`@`localhost`;
