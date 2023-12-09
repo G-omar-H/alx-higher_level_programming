dd92efea32848cdc45f6003c62b5e2fd73c7ca2e
G-omar-H <omarghazi12@gmail.com>
2023-12-09 22:16:21 +0100

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,4 @@
 SELECT `ts`.`title`, `tg`.`name` FROM `tv_shows` `ts` 
 LEFT JOIN `tv_show_genres` `tsg` ON `ts`.`id` = `tsg`.`genre_id`
 LEFT  JOIN `tv_genres` `tg`ON `tsg`.`genre_id` = `tg`.`id`
-GROUP BY `ts`.`title`
 ORDER BY `ts`.`title` ASC, `tg`.`name` ASC;
