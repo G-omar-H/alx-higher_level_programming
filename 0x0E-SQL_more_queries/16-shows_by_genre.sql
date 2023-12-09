96bbc25c66ea788e9b15e001908b4e48009ec78b
G-omar-H <omarghazi12@gmail.com>
2023-12-09 22:20:12 +0100

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@
 -- Each record should display: tv_shows.title - tv_genres.name
 -- Results must be sorted in ascending order by the show title and genre name
 SELECT `ts`.`title`, `tg`.`name` FROM `tv_shows` `ts` 
-LEFT JOIN `tv_show_genres` `tsg` ON `ts`.`id` = `tsg`.`genre_id`
+LEFT JOIN `tv_show_genres` `tsg` ON `ts`.`id` = `tsg`.`show_id`
 LEFT  JOIN `tv_genres` `tg`ON `tsg`.`genre_id` = `tg`.`id`
 ORDER BY `ts`.`title` ASC, `tg`.`name` ASC;
