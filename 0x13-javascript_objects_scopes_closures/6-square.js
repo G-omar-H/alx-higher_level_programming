0f6fcf5e2ceea25e4062fa27b41fb2e438278940
G-omar-H <omarghazi12@gmail.com>
2023-12-13 11:43:08 +0100

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    let X = 'X';
-    if (c) {
-      X = 'c';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(X.repeat(this.width));
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
