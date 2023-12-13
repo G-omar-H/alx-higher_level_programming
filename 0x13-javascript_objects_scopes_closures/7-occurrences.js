dc4b08fe4935e8087e4b41314d3310b71c5192cc
G-omar-H <omarghazi12@gmail.com>
2023-12-13 17:32:10 +0100

    7. Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; i < list.lenght; i++) {
-    if (list[i] === searchElement) {
-      count += 1;
-    }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) { count++; }
   }
   return count;
 };
