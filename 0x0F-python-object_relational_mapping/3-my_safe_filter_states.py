3c99f6660eeb4274cff9a181c1fd97cb7a0b239f
G-omar-H <omarghazi12@gmail.com>
2023-12-16 19:22:03 +0100

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states WHERE\
-                name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+                name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
