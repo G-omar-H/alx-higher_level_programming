09c56120e4e7b24b7f657ca8dc68ccc813ed0fd2
G-omar-H <omarghazi12@gmail.com>
2023-12-22 23:20:44 +0100

    15. City relationship

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -28,5 +28,3 @@ class State(Base):
                 autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    cities = relationship('City', backref='state',
-                          cascade="all, delete, delete-orphan")
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,7 +12,7 @@
             maximum 128 characters and can't be null
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
