865a37a636df87a2dab5b3ada8f87091cecf3e61
G-omar-H <omarghazi12@gmail.com>
2023-12-24 20:57:36 +0100

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv[2] === undefined || process.argv[2].match(/^([0-9]+)\.?([0-9]+)?$/) === null) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', process.argv[2]);
+  console.log('My number:', process.argv[2]);
 }
