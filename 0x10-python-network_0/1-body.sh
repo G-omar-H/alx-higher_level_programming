f6331c925f6295d38824ac7f46a1ce4ca282295e
G-omar-H <omarghazi12@gmail.com>
2024-01-06 19:44:37 +0100

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to displays onlybody of the response of 200 status
-if [ $(($(curl -o /dev/null -L -s -w "%{http_code}" "$1"))) -eq 200 ]; then curl  -LX GET "$1"; fi
+curl -sL "$1"
