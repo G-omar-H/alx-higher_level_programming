ff34e8056adc7e7cc23eb4206694090a9aa55a24
G-omar-H <omarghazi12@gmail.com>
2024-01-12 00:11:50 +0100

    FULL STACK

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request to the URL and displays
-"""
+""" module doc """
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
 if __name__ == "__main__":
     try:
+        url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
-            print(response.read().decode("utf-8"))
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
