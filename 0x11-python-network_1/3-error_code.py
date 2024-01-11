a3f931a020cc9fa17891b6f8b4870b10700942b3
G-omar-H <omarghazi12@gmail.com>
2024-01-12 00:10:42 +0100

    FULL STACK

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,9 +7,9 @@ import sys
 
 
 url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        print(response.read().decode("utf-8"))
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
