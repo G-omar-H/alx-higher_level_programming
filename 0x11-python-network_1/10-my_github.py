a11c5929eb8410e11a83c5f20884481fdb32d5c4
G-omar-H <omarghazi12@gmail.com>
2024-01-12 00:05:08 +0100

    FULL STACK

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Python script that takes your Github credentials (username and password) and
 """
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Python script that takes in a URL and an email address, sends a POST request
 """
