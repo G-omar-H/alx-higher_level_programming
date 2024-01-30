02b5fdaa96c8be5dea9ed8797b9e3848f20a6f11
G-omar-H <omarghazi12@gmail.com>
2024-01-30 21:32:40 +0100

    6. How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/**
+ * script that computes the number of tasks completed by user id.
+ */
+
+const req = require('request');
+
+const url = process.argv[2];
+
+req(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const content = JSON.parse(body);
+  const raport = {};
+  let count = 0;
+  let id = 1;
+  for (const i in content) {
+    if (content[i].userId === id) {
+      if (content[i].completed === true) {
+        count += 1;
+        raport[content[i].userId] = count;
+      }
+    } else {
+      id += 1;
+      count = 0;
+    }
+  }
+  console.log(raport);
+});
